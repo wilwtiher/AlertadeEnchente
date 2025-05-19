a2305900686ab43663aefdc479d336f8b439c784
wilwtiher <wiltonjrtablet@gmail.com>
2025-05-19 16:58:37 -0300

    porcentagem

--- AlertadeEnchente.c
+++ AlertadeEnchente.c
@@ -20,8 +20,16 @@
 #define ADC_JOYSTICK_X 26
 #define ADC_JOYSTICK_Y 27
 #define LED_BLUE 12
-#define LED_GREEN  11
+#define LED_GREEN 11
 #define tam_quad 10
+
+typedef struct
+{
+    uint16_t x_pos;
+    uint16_t y_pos;
+} joystick_data_t;
+
+QueueHandle_t xQueueJoystickData;
 
 // Matriz de LEDs
 #define IS_RGBW false
@@ -34,30 +42,18 @@
 #define botao_pinA 5 // Botão A = 5, Botão B = 6 , BotãoJoy = 22
 
 // Variaveis globais
-bool Noturno = false;
-const char* cores[] = {
-    "Verde",
-    "Amare",
-    "Verme",
-    "Notur"
-};
 uint8_t estado = 0;
 bool led_buffer[2][NUM_PIXELS] = {
-    {
-        0, 0, 0, 0, 0, 
-        0, 0, 0, 0, 0, 
-        0, 0, 1, 0, 0, 
-        0, 0, 1, 0, 0, 
-        0, 0, 0, 0, 0  
-    },
-    {
-        0, 0, 1, 0, 0, 
-        0, 1, 0, 1, 0, 
-        1, 0, 1, 0, 1, 
-        1, 0, 0, 0, 1, 
-        1, 1, 1, 1, 1   
-    }
-};
+    {0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0,
+     0, 0, 1, 0, 0,
+     0, 0, 1, 0, 0,
+     0, 0, 0, 0, 0},
+    {0, 0, 1, 0, 0,
+     0, 1, 0, 1, 0,
+     1, 0, 1, 0, 1,
+     1, 0, 0, 0, 1,
+     1, 1, 1, 1, 1}};
 
 // Funcoes para serem chamadas
 // Funções para matriz LEDS
@@ -90,146 +86,116 @@ void set_one_led(uint8_t r, uint8_t g, uint8_t b)
     }
 }
 // Funcoes de programa do FreeRTOS
-//funcao para acender semaforo
-void vSemaforoTask()
+void vJoystickTask(void *params)
 {
-    // configuracao do PIO
-    PIO pio = pio0;
-    int sm = 0;
-    uint offset = pio_add_program(pio, &ws2812_program);
+    adc_gpio_init(ADC_JOYSTICK_Y);
+    adc_gpio_init(ADC_JOYSTICK_X);
+    adc_init();
 
-    ws2812_program_init(pio, sm, offset, WS2812_PIN, 800000, IS_RGBW);
-    //iniciacao pinos
-    gpio_init(led_RED);
-    gpio_set_dir(led_RED, GPIO_OUT);
-    gpio_init(led_GREEN);
-    gpio_set_dir(led_GREEN, GPIO_OUT);
-    gpio_put(led_RED, false);
-    gpio_put(led_GREEN, false);
-    //iniciacao buzzer
-    gpio_set_function(buzzer, GPIO_FUNC_PWM);
-    uint slice_num = pwm_gpio_to_slice_num(buzzer);
-    pwm_set_wrap(slice_num, 4096);
-    // Define o clock divider como 440 (nota lá para o buzzer)
-    pwm_set_clkdiv(slice_num, 440.0f);
-    pwm_set_enabled(slice_num, true);
+    joystick_data_t joydata;
 
     while (true)
     {
-        //sinal verde
-        if (!Noturno)
-        {
-            estado = 0;
-            set_one_led(0, 5, 0);
-            gpio_put(led_GREEN, true);
-            gpio_put(led_RED, false);
-            pwm_set_gpio_level(buzzer, 2048);
-            vTaskDelay(pdMS_TO_TICKS(150));
-            pwm_set_gpio_level(buzzer, 0);
-            vTaskDelay(pdMS_TO_TICKS(1500));
-        }
-        //sinal amarelo
-        if (!Noturno)
-        {
-            estado = 1;
-            set_one_led(5, 5, 0);
-            gpio_put(led_RED, true);
-            gpio_put(led_GREEN, true);
-            for (int i = 0; i < 4; i++)
-            {
-                pwm_set_gpio_level(buzzer, 2048);
-                vTaskDelay(pdMS_TO_TICKS(200));
-                pwm_set_gpio_level(buzzer, 0);
-                vTaskDelay(pdMS_TO_TICKS(300));
-            }
-            vTaskDelay(pdMS_TO_TICKS(1250));
-        }
-        //sinal vermelho
-        if (!Noturno)
-        {
-            estado = 2;
-            set_one_led(5, 0, 0);
-            gpio_put(led_GREEN, false);
-            gpio_put(led_RED, true);
-            pwm_set_gpio_level(buzzer, 2048);
-            vTaskDelay(pdMS_TO_TICKS(500));
-            pwm_set_gpio_level(buzzer, 0);
-            vTaskDelay(pdMS_TO_TICKS(1500));
-            gpio_put(led_RED, false);
-        }
-        //modo noturno
-        if (Noturno)
-        {
-            estado = 3;
-            set_one_led(5, 5, 0);
-            pwm_set_gpio_level(buzzer, 2048);
-            gpio_put(led_RED, true);
-            gpio_put(led_GREEN, true);
-            vTaskDelay(pdMS_TO_TICKS(500));
-            set_one_led(0, 0, 0);
-            pwm_set_gpio_level(buzzer, 0);
-            gpio_put(led_RED, false);
-            gpio_put(led_GREEN, false);
-            vTaskDelay(pdMS_TO_TICKS(2000));
-        }
+        adc_select_input(0); // GPIO 26 = ADC0
+        joydata.y_pos = adc_read();
+
+        adc_select_input(1); // GPIO 27 = ADC1
+        joydata.x_pos = adc_read();
+
+        xQueueSend(xQueueJoystickData, &joydata, 0); // Envia o valor do joystick para a fila
+        vTaskDelay(pdMS_TO_TICKS(100));              // 10 Hz de leitura
     }
 }
-//funcao para verificar botao A
-void vModoTask()
+
+void vDisplayTask(void *params)
 {
-    gpio_init(botao_pinA);
-    gpio_set_dir(botao_pinA, GPIO_IN);
-    gpio_pull_up(botao_pinA);
-    while (true)
-    {
-        if (gpio_get(botao_pinA))
-        {
-            Noturno = false;
-            vTaskDelay(pdMS_TO_TICKS(50));
-        }
-        else
-        {
-            Noturno = true;
-            vTaskDelay(pdMS_TO_TICKS(50));
-        }
-    }
-}
-//funcao para modificar o display
-void vDisplay3Task()
-{
-    // I2C Initialisation. Using it at 400Khz.
     i2c_init(I2C_PORT, 400 * 1000);
+    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
+    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
+    gpio_pull_up(I2C_SDA);
+    gpio_pull_up(I2C_SCL);
 
-    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);                    // Set the GPIO pin function to I2C
-    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);                    // Set the GPIO pin function to I2C
-    gpio_pull_up(I2C_SDA);                                        // Pull up the data line
-    gpio_pull_up(I2C_SCL);                                        // Pull up the clock line
-    ssd1306_t ssd;                                                // Inicializa a estrutura do display
-    ssd1306_init(&ssd, WIDTH, HEIGHT, false, endereco, I2C_PORT); // Inicializa o display
-    ssd1306_config(&ssd);                                         // Configura o display
-    ssd1306_send_data(&ssd);                                      // Envia os dados para o display
-    // Limpa o display. O display inicia com todos os pixels apagados.
-    ssd1306_fill(&ssd, false);
+    ssd1306_t ssd;
+    ssd1306_init(&ssd, WIDTH, HEIGHT, false, endereco, I2C_PORT);
+    ssd1306_config(&ssd);
     ssd1306_send_data(&ssd);
 
+    joystick_data_t joydata;
     bool cor = true;
+    char str_x[5]; // Buffer para armazenar a string
+    char str_y[5]; // Buffer para armazenar a string
+    float porcent_x, porcent_y;
     while (true)
     {
-        ssd1306_fill(&ssd, !cor);                            // Limpa o display
-        ssd1306_rect(&ssd, 3, 3, 122, 60, cor, !cor);        // Desenha um retângulo
-        ssd1306_line(&ssd, 3, 25, 123, 25, cor);             // Desenha uma linha
-        ssd1306_line(&ssd, 3, 37, 123, 37, cor);             // Desenha uma linha
-        ssd1306_draw_string(&ssd, "CEPEDI   TIC37", 8, 6);   // Desenha uma string
-        ssd1306_draw_string(&ssd, "EMBARCATECH", 20, 16);    // Desenha uma string
-        ssd1306_draw_string(&ssd, "FreeRTOS - Wil", 10, 28);     // Desenha uma string
-        ssd1306_draw_string(&ssd, "   SEMAFORO", 10, 41); // Desenha uma string
-        ssd1306_draw_string(&ssd, cores[estado], 44, 52);            // Desenha uma string
-        ssd1306_send_data(&ssd);                             // Atualiza o display
-        sleep_ms(735);
+        if (xQueueReceive(xQueueJoystickData, &joydata, portMAX_DELAY) == pdTRUE)
+        {
+            porcent_x = (int16_t)joydata.x_pos * 0.024;
+            porcent_y = (int16_t)joydata.y_pos * 0.024;
+            sprintf(str_x, "%1.0f", porcent_x);               // Converte o inteiro em string
+            sprintf(str_y, "%1.0f", porcent_y);               // Converte o float em string
+            ssd1306_fill(&ssd, !cor);                         // Limpa o display
+            ssd1306_rect(&ssd, 3, 3, 122, 60, cor, !cor);     // Desenha um retângulo
+            ssd1306_line(&ssd, 3, 25, 123, 25, cor);          // Desenha uma linha
+            ssd1306_line(&ssd, 3, 37, 123, 37, cor);          // Desenha uma linha
+            ssd1306_draw_string(&ssd, "Monitoramento", 8, 6); // Desenha uma string
+            ssd1306_draw_string(&ssd, "Chuva:", 10, 16);      // Desenha uma string
+            ssd1306_draw_string(&ssd, str_x, 60, 16);         // Desenha uma string
+            ssd1306_draw_string(&ssd, "Agua:", 10, 28);       // Desenha uma string
+            ssd1306_draw_string(&ssd, str_y, 60, 28);         // Desenha uma string
+            ssd1306_send_data(&ssd);                          // Atualiza o display
+        }
     }
 }
 
-// Trecho para modo BOOTSEL com botão B
+void vLedGreenTask(void *params)
+{
+    gpio_set_function(LED_GREEN, GPIO_FUNC_PWM);   // Configura GPIO como PWM
+    uint slice = pwm_gpio_to_slice_num(LED_GREEN); // Obtém o slice de PWM
+    pwm_set_wrap(slice, 100);                      // Define resolução (0–100)
+    pwm_set_chan_level(slice, PWM_CHAN_B, 0);      // Duty inicial
+    pwm_set_enabled(slice, true);                  // Ativa PWM
+
+    joystick_data_t joydata;
+    while (true)
+    {
+        if (xQueueReceive(xQueueJoystickData, &joydata, portMAX_DELAY) == pdTRUE)
+        {
+            // Brilho proporcional ao desvio do centro
+            int16_t desvio_centro = (int16_t)joydata.x_pos - 2000;
+            if (desvio_centro < 0)
+                desvio_centro = -desvio_centro;
+            uint16_t pwm_value = (desvio_centro * 100) / 2048;
+            pwm_set_chan_level(slice, PWM_CHAN_B, pwm_value);
+        }
+        vTaskDelay(pdMS_TO_TICKS(50)); // Atualiza a cada 50ms
+    }
+}
+
+void vLedBlueTask(void *params)
+{
+    gpio_set_function(LED_BLUE, GPIO_FUNC_PWM);   // Configura GPIO como PWM
+    uint slice = pwm_gpio_to_slice_num(LED_BLUE); // Obtém o slice de PWM
+    pwm_set_wrap(slice, 100);                     // Define resolução (0–100)
+    pwm_set_chan_level(slice, PWM_CHAN_A, 0);     // Duty inicial
+    pwm_set_enabled(slice, true);                 // Ativa PWM
+
+    joystick_data_t joydata;
+    while (true)
+    {
+        if (xQueueReceive(xQueueJoystickData, &joydata, portMAX_DELAY) == pdTRUE)
+        {
+            // Brilho proporcional ao desvio do centro
+            int16_t desvio_centro = (int16_t)joydata.y_pos - 2048;
+            if (desvio_centro < 0)
+                desvio_centro = -desvio_centro;
+            uint16_t pwm_value = (desvio_centro * 100) / 2048;
+            pwm_set_chan_level(slice, PWM_CHAN_A, pwm_value);
+        }
+        vTaskDelay(pdMS_TO_TICKS(50)); // Atualiza a cada 50ms
+    }
+}
+
+// Modo BOOTSEL com botão B
 #include "pico/bootrom.h"
 #define botaoB 6
 void gpio_irq_handler(uint gpio, uint32_t events)
@@ -239,21 +205,23 @@ void gpio_irq_handler(uint gpio, uint32_t events)
 
 int main()
 {
-    // Para ser utilizado o modo BOOTSEL com botão B
+    // Ativa BOOTSEL via botão
     gpio_init(botaoB);
     gpio_set_dir(botaoB, GPIO_IN);
     gpio_pull_up(botaoB);
     gpio_set_irq_enabled_with_callback(botaoB, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
-    // Fim do trecho para modo BOOTSEL com botão B
 
     stdio_init_all();
-    //chamadas de funcoes
-    xTaskCreate(vSemaforoTask, "Semaforo Task", configMINIMAL_STACK_SIZE,
-                NULL, tskIDLE_PRIORITY, NULL);
-    xTaskCreate(vDisplay3Task, "Cont Task Disp3", configMINIMAL_STACK_SIZE,
-                NULL, tskIDLE_PRIORITY, NULL);
-    xTaskCreate(vModoTask, "Modo norturno Task", configMINIMAL_STACK_SIZE,
-                NULL, tskIDLE_PRIORITY, NULL);
-    vTaskStartScheduler(); // comeca as chamadas das funcoes
-    panic_unsupported(); // caso aconteca algum desastre com o programa
+
+    // Cria a fila para compartilhamento de valor do joystick
+    xQueueJoystickData = xQueueCreate(5, sizeof(joystick_data_t));
+
+    // Criação das tasks
+    xTaskCreate(vJoystickTask, "Joystick Task", 256, NULL, 1, NULL);
+    xTaskCreate(vDisplayTask, "Display Task", 512, NULL, 1, NULL);
+    xTaskCreate(vLedGreenTask, "LED red Task", 256, NULL, 1, NULL);
+    xTaskCreate(vLedBlueTask, "LED blue Task", 256, NULL, 1, NULL);
+    // Inicia o agendador
+    vTaskStartScheduler();
+    panic_unsupported();
 }
